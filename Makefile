691d24752180f98a9d4a338e0291ad57bb06b926
Ariss <valentina.ways@yandex.ru>
2020-06-19 15:41:22 +0300

    sniffer created, makefile updated

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 install:
 	composer install
+	
+make lint:
+	composer run-script phpcs -- --standard=PSR12 src bin
+
--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,14 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * Greeting in the game with player's name
+ *
+ * @return nothing;
+ * */
 function run()
 {
-  line('Welcome to the Brain Game!');
-  $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
