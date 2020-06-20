9693e2166d0cb1893507bb8ccf1a4c604b7dff57
Ariss <valentina.ways@yandex.ru>
2020-06-20 19:15:16 +0300

    The game about even numbers relized

---
+++ src/BrainEvenCode.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * It's all about one of brain game where a player has to indicate an even numbers.
+ *
+ * @author valentina-vasileva <valentina.vasileva@yandex.ru>
+ */
+
+namespace BrainGames\BrainEvenCode;
+
+use function cli\line;
+use function cli\prompt;
+
+/**
+ * The one of brain games. The goal is indicating an even number for three times.
+ *
+ * @param string $name Name of player
+ *
+ * @return nothing
+ * */
+function brainEven($name)
+{
+    for ($index = 0; $index <= 2; $index++) {
+        $number = rand();
+        line('Question: %d', $number);
+        $answer = prompt('Your answer');
+        if ($number % 2 === 0 && $answer === 'yes') {
+            line('Correct!');
+            if ($index === 2) {
+                line("Congratulations, %s!", $name);
+            }
+        } elseif ($number % 2 === 0 && $answer !== 'yes') {
+            line("'%s' is wrong answer ;(. Correct answer was 'yes'.", $answer);
+            line("Let's try again, %s!", $name);
+            break;
+        } elseif ($number % 2 !== 0 && $answer === 'no') {
+            line('Correct!');
+            if ($index === 2) {
+                line("Congratulations, %s!", $name);
+            }
+        } elseif ($number % 2 !== 0 && $answer !== 'no') {
+            line("'%s' is wrong answer ;(. Correct answer was 'no'.", $answer);
+            line("Let's try again, %s!", $name);
+            break;
+        }
+    }
+}
--- src/Cli.php
+++ src/Cli.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * String functions for brain games.
+ *
+ * @author valentina-vasileva <valentina.vasileva@yandex.ru>
+ */
+
 namespace BrainGames\Cli;
 
 use function cli\line;
@@ -8,11 +14,12 @@ use function cli\prompt;
 /**
  * Greeting in the game with player's name
  *
- * @return nothing;
+ * @return $name
  * */
 function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
