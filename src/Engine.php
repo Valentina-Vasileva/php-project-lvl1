de4ad6dfbc79639fbdc9efc15e86b3c069612eb0
Ariss <valentina.ways@yandex.ru>
2020-07-06 19:31:28 +0300

    Engine united into one part

--- src/Engine.php
+++ src/Engine.php
@@ -8,43 +8,6 @@ use function cli\prompt;
 const LAST_GAME = 3;
 
 /**
- * Welcome part of brain games.
- *
- * @return nothing
- */
-function welcomeToTheBrainGame()
-{
-    line();
-    line('Welcome to the Brain Game!');
-}
-
-/**
- * Shows instructions of a brain game.
- *
- * @param $instructions Instructions of a brain game
- *
- * @return nothing
- */
-function showInstructions($instructions)
-{
-    line('%s', $instructions);
-    line();
-}
-
-/**
- * Gets the name of player and greets.
- *
- * @return $name Name of player
- */
-function getNameOfPlayerAndHello()
-{
-    $name = prompt('May I have your name?', false, '');
-    line("Hello, %s!", $name);
-    line();
-    return $name;
-}
-
-/**
  * The engine of brain games.
  *
  * @param $questionAndAnswer Array of questions to player and right answers $instructions Instructions for a brain game
@@ -53,9 +16,13 @@ function getNameOfPlayerAndHello()
  * */
 function engineBrainGame(array $questionAndAnswer, string $instructions)
 {
-    welcomeToTheBrainGame();
-    showInstructions($instructions);
-    $name = getNameOfPlayerAndHello();
+    line();
+    line('Welcome to the Brain Game!');
+    line('%s', $instructions);
+    line();
+    $name = prompt('May I have your name?', false, '');
+    line("Hello, %s!", $name);
+    line();
     foreach ($questionAndAnswer as $question => $rightAnswer) {
         line('Question: %s', $question);
         $answer = prompt('Your answer');
